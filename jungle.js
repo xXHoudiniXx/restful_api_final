f046bc5c251894c3dee9e50578488c9fc787339d
dbarrett <jbarrett20111978@gmail.com>
2018-12-06 14:11:48 +0000

    update

---
+++ jungle.js
@@ -0,0 +1,61 @@
+db.product.drop();
+
+db.product.insert({
+  name: "Batman Lego Set",
+  qty: 1,
+  price: 9.99,
+  company: "LEGO",
+  category: "Toys"
+});
+
+db.product.insert({
+  name: "32 Piece Ratchet Set",
+  qty: 2,
+  price: 29.99,
+  company: "Stanley",
+  category: "Tools"
+});
+
+db.product.insert({
+  name: "Hammer",
+  qty: 5,
+  price: 5.99,
+  company: "Stanley",
+  category: "Tools"
+});
+
+db.product.insert({
+  name: "Toothpaste",
+  qty: 1,
+  price: 2.99,
+  company: "Crest",
+  category: "Health"
+});
+
+db.product.insert({
+  name: "Floss",
+  qty: 0,
+  price: .99,
+  company: "Crest",
+  category: "Health"
+});
+
+db.product.insert({
+  name: "Power Drill",
+  qty: 5,
+  price: 49.99,
+  category: "Tools"
+});
+
+db.product.insert({
+  name: "120 Piece Lego Set",
+  qty: 1,
+  price: 12.99,
+  company: "LEGO"
+});
+
+db.product.insert({
+  name: "Hot-wheels Car",
+  qty: 10,
+  price: .99
+});
